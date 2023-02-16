06a4d7c8ece43c37d9d5cf54cd0a7aa932133734
lee <cocoball.mohae@gmail.com>
2023-02-16 16:56:10 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <h1>리액트 안녕</h1>
       <h2>잘 작동하나</h2>
-    </div>
+    </Fragment>
   );
 }
 
